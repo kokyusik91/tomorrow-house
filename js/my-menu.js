9f9ec32454dd4cad11a7ca412fb3726e650397fa
kks91 <kks91@dgdr.co.kr>
2022-07-21 06:13:05 +0900

    🚧 Implement add my menus

---
+++ js/my-menu.js
@@ -0,0 +1,18 @@
+const myMenu = document.querySelector('.my-menu')
+const myMenuButton = document.querySelector('.my-menu-button')
+
+function toggleMyMenu() {
+  myMenu.classList.toggle('is-active')
+}
+
+myMenuButton.addEventListener('click', toggleMyMenu)
+
+// 마이메뉴 이외의 영역을 클릭했을때 닫히게
+// 누를 대상이 명확하지 않다.. 나머지 요소들에게 다 걸어줄수 없다
+// window에게 걸어준다.
+
+window.addEventListener('click', function (e) {})
+
+// 1. 내가 클릭한 요소가 무엇인지? => e.target
+// 2. myMenu가 요소를 포함하고 있는지를 알 수 있는 방법
+// contains
