5826cc1db20f0fbd95d3652327509ef00693e1b3
kks91 <kks91@dgdr.co.kr>
2022-07-21 07:10:43 +0900

    🚧 Implement deleting all searchHistory items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,8 +1,17 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
 
 function openGnbSearchHistory() {
+  // 체크 => gnbSearchHistoryList안에 li가 몇개 있는지
+  // li가 0개 => 실행 X
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
@@ -16,4 +25,12 @@ function closeGnbSearchHistory(e) {
   }
 }
 
+function deleteAllSearchHistories() {
+  // gnbSearchHistoryList 안에 들어 있는 모든 li를 삭제
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistories)
