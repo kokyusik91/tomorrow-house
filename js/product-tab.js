c55a0dc9ea5395825c13b9b14c1e736332153518
kks91 <kks91@dgdr.co.kr>
2022-07-21 09:40:47 +0900

    💫 Scroll to tab-panel when product-tab moved

--- js/product-tab.js
+++ js/product-tab.js
@@ -3,6 +3,9 @@ const productTab = document.querySelector('.product-tab')
 
 const productTabButtonList = productTab.querySelectorAll('button')
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
+
 // 현재 active 되있는 정보를 업데이트 해줌
 let currentActiveTab = productTab.querySelector('.is-active')
 
@@ -18,6 +21,24 @@ function toggleActiveTab() {
   }
 }
 
+function scrollToTabPanel() {
+  // product-tab-item에 연결된 aria-labelledby 값으로 어느 tabpanel에 연결되어있는지 확인!
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`) // #product-review
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+    // 얼마나 스크롤을 시켜야 하는가.
+    behavior: 'smooth',
+  })
+}
+
 productTabButtonList.forEach((button) => {
+  // 두개의 이벤트 핸들러가 동작됨. 다른 함수로 분할해 놓는게 좋음...
   button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollToTabPanel)
 })
